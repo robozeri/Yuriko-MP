71bed06f01fd27fbcb447dcc123a1d2c70106277
Manuel Ficara <manuelficara01@gmail.com>
2015-12-02 15:21:46 +0000

    Updated BrewingStand

--- src/pocketmine/block/BrewingStand.php
+++ src/pocketmine/block/BrewingStand.php
@@ -1,46 +1,72 @@
 <?php
+ 
+ /*
+  *
+  *  ____            _        _   __  __ _                  __  __ ____  
+  * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \ 
+  * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
+  * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/ 
+  * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_| 
+  *
+  * This program is free software: you can redistribute it and/or modify
+  * it under the terms of the GNU Lesser General Public License as published by
+  * the Free Software Foundation, either version 3 of the License, or
+  * (at your option) any later version.
+  *
+  * @author PocketMine Team
+  * @link http://www.pocketmine.net/
+  * 
+  *
+ */
+ 
+ namespace pocketmine\block;
+ 
+ use pocketmine\inventory\BrewingstandInventory;
+ use pocketmine\item\Item;
+ use pocketmine\item\Tool;
+ use pocketmine\Player;
+ 
+ class BrewingStand extends Transparent{
+ 
+ 	protected $id = self::BREWING_STAND;
+ 
+ 	public function __construct(){
+ 
+ 	}
+ 
+ 	public function getName(){
+ 		return "Brewing Stand";
+ 	}
+ 
+ 	public function getHardness(){
+ 		return 0.5;
+ 	}
+ 
+ 	public function getToolType(){
+ 		return Tool::TYPE_PICKAXE;
+ 	}
 
-/*
- *
- *  ____            _        _   __  __ _                  __  __ ____  
- * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \ 
- * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
- * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/ 
- * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_| 
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * @author PocketMine Team
- * @link http://www.pocketmine.net/
- * 
- *
-*/
+        public function canBeActivated(){
+                return true;
+        }
 
-namespace pocketmine\block;
-
-use pocketmine\item\Item;
-use pocketmine\item\Tool;
-
-class BrewingStand extends Transparent{
-
-	protected $id = self::BREWING_STAND;
-
-	public function __construct(){
-
+        public function onActivate(Item $item, Player $player = null){
+		if($player instanceof Player){
+			if($player->isCreative()){
+				return true;
+			}
+			$player->addWindow(new BrewingstandInventory($this));
+		}
+		return true;
 	}
 
-	public function getName(){
-		return "Brewing Stand";
-	}
-
-	public function getHardness(){
-		return 0.5;
-	}
-
-	public function getToolType(){
-		return Tool::TYPE_PICKAXE;
+        public function getDrops(Item $item){
+		if($item->isPickaxe() >= Tool::TIER_WOODEN){
+			return [
+				[$this->id, 0, 1],
+			];
+		}else{
+			return [];
+		}
 	}
 }
