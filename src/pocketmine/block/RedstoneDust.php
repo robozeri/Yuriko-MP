26b8ac19c3447edc1d833e50a33cc7df9e847ec7
luca28pet <lucapetrucci28@yahoo.it>
2015-12-08 18:15:44 +0100

    Other work on redstone

--- src/pocketmine/block/RedstoneDust.php
+++ src/pocketmine/block/RedstoneDust.php
@@ -24,18 +24,19 @@ class RedstoneDust extends Flowable implements RedPowerConductor{
         if($type === Level::BLOCK_UPDATE_NORMAL){
             $powered = false;
             for($s = 0; $s <= 6; $s++){
-                $powered = ($this->getSide($s) instanceof RedPowerSource);
+                $sideBlock = $this->getSide($s);
+                $powered = ($sideBlock instanceof RedPowerSource and $sideBlock->getPower() > $this->getPower());
             }
             if(!$powered){
                 $this->setPower(0);
                 $this->setActivated(false);
-                $this->level->updateAround($this);
+                //$this->level->updateAround($this);
             }
 
             for($s = 0; $s <= 6; $s++){
                 $sideBlock = $this->getSide($s);
                 if($sideBlock instanceof RedPowerConductor and $powered){
-                    if($sideBlock->getPower() <= $this->getPower()){
+                    if($sideBlock->getPower() <= $this->getPower() and $this->getPower() > 0){
                         $sideBlock->setPower($this->getPower() - 1);
                     }
                 }elseif($sideBlock instanceof RedPowerConsumer){
@@ -44,6 +45,7 @@ class RedstoneDust extends Flowable implements RedPowerConductor{
                     }
                 }
             }
+            $this->level->setBlock($this, $this);
         }
     }
 
