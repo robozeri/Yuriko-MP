ff17229015ad7356e74f3a006a0631d79aff7f04
luca28pet <lucapetrucci28@yahoo.it>
2015-12-08 22:37:32 +0100

    More redstone work

--- src/pocketmine/block/RedstoneDust.php
+++ src/pocketmine/block/RedstoneDust.php
@@ -2,14 +2,16 @@
 
 namespace pocketmine\block;
 
+use pocketmine\item\Item;
 use pocketmine\level\Level;
+use pocketmine\Player;
 
 class RedstoneDust extends Flowable implements RedPowerConductor{
     protected $id = self::REDSTONE_DUST;
     protected $activated = false;
 
-    public function __construct(){
-
+    public function __construct($meta = 0){
+        $this->meta = $meta;
     }
 
     public function getPower(){
@@ -25,27 +27,36 @@ class RedstoneDust extends Flowable implements RedPowerConductor{
             $powered = false;
             for($s = 0; $s <= 6; $s++){
                 $sideBlock = $this->getSide($s);
-                $powered = ($sideBlock instanceof RedPowerSource and $sideBlock->getPower() > $this->getPower());
-            }
-            if(!$powered){
-                $this->setPower(0);
-                $this->setActivated(false);
-                //$this->level->updateAround($this);
-            }
-
-            for($s = 0; $s <= 6; $s++){
-                $sideBlock = $this->getSide($s);
-                if($sideBlock instanceof RedPowerConductor and $powered){
-                    if($sideBlock->getPower() <= $this->getPower() and $this->getPower() > 0){
-                        $sideBlock->setPower($this->getPower() - 1);
-                    }
-                }elseif($sideBlock instanceof RedPowerConsumer){
-                    if($powered and !$sideBlock->isActivated()){
-                        $sideBlock->setActivated(true);
-                    }
+                if($sideBlock instanceof RedPowerSource and $sideBlock->getPower() >= $this->getPower()){
+                    $powered = $sideBlock->getPower();
                 }
             }
-            $this->level->setBlock($this, $this);
+
+            if($powered){ //Is receiving power (> 0)
+                $this->setPower($powered);
+                if(!$this->isActivated()){
+                    $this->setActivated(true);
+                }
+                for($s = 0; $s <= 6; $s++){
+                    $sideBlock = $this->getSide($s);
+                    if($sideBlock instanceof RedPowerConductor){
+                        if($sideBlock->getPower() <= $this->getPower() and $this->getPower() > 1){
+                            $sideBlock->setPower($this->getPower() - 1);
+                        }
+                    }elseif($sideBlock instanceof RedPowerConsumer and !($sideBlock instanceof RedPowerSource)){
+                        if(!$sideBlock->isActivated()){
+                            $sideBlock->setActivated(true);
+                        }
+                    }
+                }
+            }else{
+                $this->setPower(0);
+                if($this->isActivated()){
+                    $this->setActivated(false);
+                }
+            }
+
+            $this->level->setBlock($this, $this, true, false); //TODO: find a way to update block around without generating a death loop
             return Level::BLOCK_UPDATE_NORMAL;
         }
         return false;
@@ -57,6 +68,13 @@ class RedstoneDust extends Flowable implements RedPowerConductor{
 
     public function setActivated($bool){
         $this->activated = $bool;
+    }
+
+    public function place(Item $item, Block $block, Block $target, $face, $fx, $fy, $fz, Player $player = null){
+        if($face === 1 and !$this->getSide(0)->isTransparent()){ //Up
+            return parent::place($item, $block, $target, $face, $fx, $fy, $fz, $player);
+        }
+        return false;
     }
 
 }
