502d550c37900b8f0c707119de01ab404f3527e8
Manuel Ficara <manuelficara01@gmail.com>
2015-12-11 10:36:01 +0000

    :P

--- src/pocketmine/block/DoubleSlab.php
+++ src/pocketmine/block/DoubleSlab.php
@@ -22,10 +22,9 @@
 namespace pocketmine\block;
 
 use pocketmine\item\Item;
-use pocketmine\item\Tool;
 
-class DoubleSlab extends Solid{
-	protected $id = self::DOUBLE_SLAB;
+class DoubleWoodSlab extends Solid{
+	protected $id = self::DOUBLE_WOOD_SLAB;
 
 	public function __construct($meta = 0){
 		$this->meta = $meta;
@@ -36,29 +35,31 @@ class DoubleSlab extends Solid{
 	}
 
 	public function getToolType(){
-		return Tool::TYPE_PICKAXE;
+		return Tool::TYPE_AXE;
 	}
 
 	public function getName(){
 		static $names = [
-			0 => "Stone",
-			1 => "Sandstone",
-			2 => "Wooden",
-			3 => "Cobblestone",
-			4 => "Brick",
-			5 => "Stone Brick",
-			6 => "Quartz",
-			7 => "Nether Brick",
+			0 => "Oak",
+			1 => "Spruce",
+			2 => "Birch",
+			3 => "Jungle",
+			4 => "Acacia",
+			5 => "Dark Oak",
+			6 => "",
+			7 => ""
 		];
-		return "Double " . $names[$this->meta & 0x07] . " Slab";
+		return "Double " . $names[$this->meta & 0x07] . " Wooden Slab";
 	}
 
 	public function getDrops(Item $item){
-		if($item->isPickaxe() >= Tool::TIER_WOODEN){
-			return [
-				[Item::SLAB, $this->meta & 0x07, 2],
-			];
-		}
-		return [];
+		return [
+			[Item::WOOD_SLAB, $this->meta & 0x07, 2],
+		];
+	}
+
+	public function isBreakable(Item $item){
+		$this->getLevel()->setBlock($this, new Air(), true, true);
+		return true;
 	}
 }
