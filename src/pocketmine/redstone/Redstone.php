a8e9b5b88cde3f3b7a0016bfa02e16e4eeba559b
luca28pet <lucapetrucci28@yahoo.it>
2015-12-27 12:53:37 +0100

    .

--- src/pocketmine/redstone/Redstone.php
+++ src/pocketmine/redstone/Redstone.php
@@ -3,71 +3,68 @@
 namespace pocketmine\redstone;
 
 use pocketmine\block\Block;
-use pocketmine\block\RedstoneDust;
-use pocketmine\Server;
 
 class Redstone{
 
     public static function active(Block $source){
         $queue = new \SplPriorityQueue();
-        $curLevel = $source->getPowerLevel() - 1;
-        if($curLevel <= 0){
+        $queue->setExtractFlags(\SplPriorityQueue::EXTR_BOTH);
+        $queue->insert($source, $source->getPowerLevel());
+        if(!$source->isPowerSource()){
             return;
         }
-        $queue->setExtractFlags(\SplPriorityQueue::EXTR_BOTH);
         self::addToQueue($queue, $source);
-        $prev = null;
         while(!$queue->isEmpty()){
             $ex = $queue->extract();
             $updating = $ex["data"];
             $power = $ex["priority"];
             /** @var Block $updating*/
-            if($power !== $updating->getPowerLevel()){
-                $updating->setPowerLevel($power);
-                $updating->level->setBlock($updating, $updating, true);
-                self::addToQueue($queue, $updating);
-            }
+            $updating->setPowerLevel($power);
+            $updating->level->setBlock($updating, $updating, true);
+            self::addToQueue($queue, $updating);
         }
     }
 
     public static function deactive(Block $source, $updateLevel){
         $queue = new \SplPriorityQueue();
-        $curLevel = $updateLevel;
-        if($curLevel <= 0){
+        $queue->setExtractFlags(\SplPriorityQueue::EXTR_BOTH);
+        $queue->insert($source, $source->getPowerLevel());
+        if(!$source->isPowerSource()){
             return;
         }
-        self::addToDeactiveQueue($queue, $source);
+        self::addToDeactiveQueue($queue, $source, $updateLevel);
         while(!$queue->isEmpty()){
-            $updating = $queue->extract();
-            if($updating->getPowerLevel() < $source->getPowerLevel()){
-                $updating->setPowerlevel(0);
-                $updating->level->setBlock($updating, $updating, true);
-                self::addToDeactiveQueue($queue, $updating);
-            }
+            $ex = $queue->extract();
+            $updating = $ex["data"];
+            $power = $ex["priority"];
+            /** @var Block $updating */
+            $updating->setPowerLevel(0);
+            $updating->level->setBlock($updating, $updating, true);
+            self::addToDeactiveQueue($queue, $updating, $power);
         }
     }
 
     public static function addToQueue(\SplPriorityQueue $queue, Block $location){
-        if($location->getPowerLevel() <= 0){
+        $updateLevel = $location->getPowerLevel() - 1;
+        if($updateLevel <= 0){
             return;
         }
-        for($s = 2; $s <= 5; $s++){
-            $updating = $location->getSide($s);
-            if($updating instanceof RedstoneDust and $updating->getPowerLevel() < $location->getPowerLevel()){
-                $queue->insert($updating, $location->getPowerLevel() - 1);
-                Server::getInstance()->getLogger()->debug("Inserted");
+        for($s = 0; $s <= 5; $s++){
+            $sideBlock = $location->getSide($s);
+            if($sideBlock->getPowerLevel() < $updateLevel){
+                $queue->insert($sideBlock, $updateLevel);
             }
         }
     }
 
-    public static function addToDeactiveQueue(\SplPriorityQueue $queue, Block $location){
-        if($location->getPowerLevel() <= 0){
+    public static function addToDeactiveQueue(\SplPriorityQueue $queue, Block $location, $updateLevel){
+        if($updateLevel <= 0){
             return;
         }
         for($s = 0; $s <= 5; $s++){
-            $updating = $location->getSide($s);
-            if($updating->getPowerLevel() > 0 and count($updating->getPowerSources()) === 1){
-                $queue->insert($updating, $location->getPowerLevel() - 1);
+            $sideBlock = $location->getSide($s);
+            if($sideBlock->getPowerLevel() === $updateLevel - 1){
+                $queue->insert($sideBlock, $sideBlock->getPowerLevel());
             }
         }
     }
