9c8a682a35c2a4180bb1a1f03d2c10fc95cfa84a
Emanuele Sacco <xemax32@gmail.com>
2015-12-28 18:55:29 +0000

    Added  Potion Of Regeneration

---
+++ src/pocketmine/item/Potion_Of_Regeneration.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+ *
+ * __   __          _ _               __  __ ____  
+ * \ \ / /   _ _ __(_) | _____       |  \/  |  _ \ 
+ *  \ V / | | | '__| | |/ / _ \ _____| |\/| | |_) |
+ *   | || |_| | |  | |   < (_) |_____| |  | |  __/ 
+ *   |_| \__,_|_|  |_|_|\_\___/      |_|  |_|_|
+ *
+ * Yuriko-MP, a kawaii-powered PocketMine-based software
+ * for Minecraft: Pocket Edition
+ * Copyright 2015 ItalianDevs4PM.
+ *
+ * This work is licensed under the Creative Commons
+ * Attribution-NonCommercial-NoDerivatives 4.0
+ * International License.
+ * 
+ *
+ * @author ItalianDevs4PM
+ * @link   http://github.com/ItalianDevs4PM
+ *
+ *
+ */
+ 
+namespace pocketmine\item;
+
+use pocketmine\event\player\PlayerItemConsumeEvent;
+use pocketmine\item\Item;
+
+class PotionOfRegeneration extends Item{
+
+$player = $e->getPlayer();
+
+	public function __construct($meta = 0, $count = 1){
+		parent::__construct(self::POTION_OF_REGENERATION, $meta, $count, "Potion Of Regeneration");
+	}
+    
+    public function OnItemConsume(PlayerItemConsumeEvent $e){
+        if($player->getInventory()->getItemOnHand(Item::POTION_OF_REGENERATION)){
+            $player->addEffect(Effect::getEffect(Effect::REGENERATION)->setDuration(120 * 20));
+        }
+    }
+} 
